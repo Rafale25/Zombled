cb91ebe03dbda6e8ca8af9ba249591598824e58b
Rafale25 <rafale25.dev@gmail.com>
2026-05-16 22:54:10 +0200

    update gigachad on screen!

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include "Context.hpp"
-// #include "Application.hpp"
-// #include "Logger.hpp"
+#include "View/menu.hpp"
 #include "uhcio.hh"
 
 int main() {
     Context& ctx = Context::instance();
     ctx.init();
 
-    // {
-    //     App appView;
-    //     // ctx.setVsync(VSYNC);
-    //     ctx.setView(appView);
-    //     ctx.run();
-    // }
+    {
+        Log::debug("menuView");
+        App menuView;
+        // ctx.setVsync(VSYNC);
+        Log::debug("setView");
+        ctx.setView(menuView);
+        Log::debug("run");
+        ctx.run();
+    }
 
     Log::debug("Main end");
     // logD("Main end");
