ffb3cdad019aa5a6b5226490ac464e3fc31fe7a0
Blackoutburst <blackoutburst@hotmail.com>
2026-05-18 01:47:36 +0200

    server V1

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,6 +1,50 @@
-#include <print>
+#include "uhcnet.hh"
 #include "uhcio.hh"
+#include "uhcstd.hh"
+
+void* clientThread(void* arg) {
+    TcpClient::It client = *(TcpClient::It*)arg;
+
+    while (client.sockfd) {
+        // Read
+    }
+
+    return NULL;
+}
+
+void serverAccept(TcpClient::It* client) {
+    Log::info("New client joined");
+    
+    Thread::It thread = Thread::start(clientThread, client);
+    Thread::join(thread);
+    
+    Log::info("Client left");
+}
+
+void* serverListen(void* arg) {
+    Log::info("Server listening");
+    
+    TcpServer::It server = *(TcpServer::It*)arg;
+
+    while (server.running) {
+        TcpServer::listen(server, serverAccept);
+    }
+
+    return NULL;
+}
 
 int main() {
-    std::print("Hello server");
+    TcpServer::It server = TcpServer::create(22222, 10);
+
+    Thread::It thread = Thread::start(serverListen, &server);
+
+    while (server.running) {
+        // Send
+    }
+
+    Thread::join(thread);
+
+    TcpServer::destroy(server);
+
+    return 0;
 }
